704dbacc5a4a310f08ae71bcaf6e574f6124c9ca
Jelmer de Ronde <jelmerderonde@gmail.com>
2020-04-08 17:19:11 +0200

    Continue work on permissions

--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -6,6 +6,29 @@ locals {
   }
 }
 
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        "ecs-tasks.amazonaws.com"
+      ]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "this" {
   name = "${var.project}-${var.env}"
   capacity_providers = ["FARGATE"]
