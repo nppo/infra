b2265ba7a466c78de72295ecc6744748d4dcd56f
Ruben-Hartog <rhartog@zilverline.com>
2020-05-25 14:17:12 +0200

    Add module for image upload bucket

---
+++ modules/image-upload-bucket/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "Project to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
+
+variable "name" {
+  description = "Bucket name"
+  type        = string
+  default     = ""
+}
