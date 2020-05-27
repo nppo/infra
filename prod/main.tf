4e1676ee128c9f6c322b58ebcdef0b22840957c8
Jelmer de Ronde <jelmerderonde@gmail.com>
2020-05-27 15:05:54 +0200

    Adds users and vpc's for acceptance and production envs

--- prod/main.tf
+++ prod/main.tf
@@ -1,6 +1,8 @@
 locals {
   project = "surfpol"
   env = "prod"
+  ipv4_eduvpn_ips = ["145.90.230.0/23", "145.101.60.0/23"]
+  ipv6_eduvpn_ips = ["2001:610:450:50::/60", "2001:610:3:2150::/60"]
 }
 
 terraform {
@@ -23,4 +25,18 @@ module "user-access" {
   users = {"fako.berkers@surfnet.nl" = ["superusers"]
            "kruys@zilverline.com" = ["superusers"]
            "rhartog@zilverline.com" = ["superusers"]}
+}
+
+module "vpc" {
+  source = "../modules/vpc"
+
+  project = local.project
+  env = local.env
+  cidr = "10.0.0.0/16"
+
+  azs = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  ipv4_eduvpn_ips = local.ipv4_eduvpn_ips
+  ipv6_eduvpn_ips = local.ipv6_eduvpn_ips
 }
