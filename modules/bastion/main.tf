e182f8dbc198833af8e272518c491629340cf692
Ruben-Hartog <rhartog@zilverline.com>
2020-11-18 11:13:47 +0100

    Encrypt EBS instances

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -79,6 +79,16 @@ resource "aws_security_group" "eduvpn_ssh" {
   }
 }
 
+resource "aws_kms_key" "bastion_encryption_key" {
+  description = "Bastion EBS encryption key"
+  tags = merge(local.common_tags, {Name="${var.project}-bastion-ebs-encryption-key"})
+}
+
+resource "aws_kms_alias" "db_encryption_key_alias" {
+  name          = "alias/bastion-ebs-encryption-key"
+  target_key_id = aws_kms_key.bastion_encryption_key.key_id
+}
+
 resource "aws_instance" "bastion-host" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -96,6 +106,11 @@ resource "aws_instance" "bastion-host" {
   iam_instance_profile = aws_iam_instance_profile.this.name
 
   user_data = templatefile("${path.module}/bastion_setup.tpl", { public_keys = var.public_keys})
+
+  root_block_device {
+    encrypted = true
+    kms_key_id = aws_kms_key.bastion_encryption_key.arn
+  }
 
   tags = merge(local.common_tags, {Name = "${var.project}-bastion"})
   volume_tags = local.common_tags
