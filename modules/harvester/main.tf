567aee32a1b5a49aff67657b9cff260c1c0b44cb
Ruben-Hartog <rhartog@zilverline.com>
2020-11-30 13:34:47 +0100

    Harvester bucket should not block public access

--- modules/harvester/main.tf
+++ modules/harvester/main.tf
@@ -62,8 +62,8 @@ POLICY
 resource "aws_s3_bucket_public_access_block" "block_harvest_content_bucket_public_access" {
   bucket = aws_s3_bucket.harvester_content_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
