91f0fbe99307e775f3493e394dff264c08a81f21
Fako Berkers <email@fakoberkers.nl>
2021-03-11 17:20:11 +0100

    Adds a cloudwatch rule to trigger sync_category_filters

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -58,6 +58,14 @@ resource "aws_cloudwatch_event_rule" "clearlogins" {
   schedule_expression = "cron(0 2 * * ? *)"
 }
 
+resource "aws_cloudwatch_event_rule" "sync_category_filters" {
+  name        = "sync_category_filters"
+  description = "Runs the sync_category_filters command every day"
+
+  # Every day at 8:00 am UTC
+  schedule_expression = "cron(0 8 * * ? *)"
+}
+
 resource "aws_cloudwatch_log_group" "this" {
   name = "/ecs/search-portal"
   retention_in_days = 14
