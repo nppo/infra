7835299f508a031b5d5041abc82576486bcf2495
Fako Berkers <email@fakoberkers.nl>
2021-09-20 14:27:08 +0200

    Creates hosted zone and records for publinova.nl

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -100,7 +100,59 @@ resource "aws_lb_listener_rule" "search-default" {
 
   condition {
     host_header {
-      values = ["fakoberkers.nl", "nppo.fakoberkers.nl"]
+      values = ["search.publinova.nl"]
     }
+  }
+}
+
+resource "aws_route53_zone" "publinova" {
+  name = "publinova.nl"
+}
+
+resource "aws_route53_record" "harvester-ip4" {
+  zone_id = aws_route53_zone.publinova.zone_id
+  name    = "harvester.publinova.nl"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.nppo.dns_name
+    zone_id                = aws_lb.nppo.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "harvester-ip6" {
+  zone_id = aws_route53_zone.publinova.zone_id
+  name    = "harvester.publinova.nl"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_lb.nppo.dns_name
+    zone_id                = aws_lb.nppo.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "search-ip4" {
+  zone_id = aws_route53_zone.publinova.zone_id
+  name    = "search.publinova.nl"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.nppo.dns_name
+    zone_id                = aws_lb.nppo.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "search-ip6" {
+  zone_id = aws_route53_zone.publinova.zone_id
+  name    = "search.publinova.nl"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_lb.nppo.dns_name
+    zone_id                = aws_lb.nppo.zone_id
+    evaluate_target_health = true
   }
 }
